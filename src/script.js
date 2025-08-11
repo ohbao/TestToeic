525b2ccbc398cdc035145e0f1c2ac7e70c4b3952
ohbao <bao1305946@gmail.com>
2025-08-11 17:09:16 +0700

    update

--- src/script.js
+++ src/script.js
@@ -24,7 +24,7 @@ function loadTest(testId) {
     console.log(`Loading test with ID: ${testId}`);
     document.querySelector('.audio-player h3').textContent = `Test ${testId}`;
     const audioSource = document.querySelector('.audio-player audio source');
-    audioSource.src = `../audio/part_1_2/ActualTest${testId.toString()}.mp3`;
+    audioSource.src = `../audio/part_3_4/ActualTest${testId.toString()}.mp3`;
     document.querySelector('.audio-player audio').load();
 
     const listImages = document.querySelectorAll('img[id^="img"]');
@@ -34,4 +34,35 @@ function loadTest(testId) {
         img.alt = `Hình ảnh câu ${imageIndex}`;
         img.id = `img${imageIndex}-id`;
     })
+}
+
+async function loadPdf() {
+   // const data = testData[];
+    //if (!data || !data.pdfUrl) return;
+    //pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+
+    const container = document.getElementById('pdf-viewer-container');
+    container.innerHTML = ''; 
+    const pdfUrl =  '../book/tomato.pdf' ;//data.pdfUrl;
+    const startPage = 10; //data.pdfPages.start;
+    const endPage = 20; //data.pdfPages.end;
+
+    try {
+        const pdf = await pdfjsLib.getDocument({url: pdfUrl, password: 'toeicbookstore'}).promise;
+        for (let i = startPage; i <= endPage; i++) {
+            const page = await pdf.getPage(i);
+            const scale = 1.5;
+            const viewport = page.getViewport({ scale });
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+            container.appendChild(canvas);
+            await page.render({ canvasContext: context, viewport }).promise;
+        }
+    } catch (error) {
+        console.error('Error: ', error);
+        container.textContent = 'Error loading PDF document.';
+    }
 }
